cfac278d35ac681333e2b10571dfd5a5cf402e4c
Adam Gazzaley <agazzaley@Adams-MacBook-Pro-2.local>
2017-05-18 10:13:00 -0700

    demo

---
+++ Assets/Script1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour {
+
+	public float rot;
+
+	// Use this for initialization
+	void Start () {
+		// happens once at the start
+	}
+
+	public void SetRotation(float f)
+	{
+		rot = f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// happens all the time
+		transform.Rotate(Vector3.up,rot);
+	}
+		
+}
---
+++ Assets/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			GameObject go = (GameObject) Instantiate (cube, new Vector3 (0f, 4f, 0f), Quaternion.identity);
+			Script1 s = go.GetComponent<Script1> ();
+			s.SetRotation (Random.value * 100f);
+		}
+	}
+}
